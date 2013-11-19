19570594e5d1f6ea747be7e38a5d820ad7527391
aerben <erben@lat-lon.de>
2013-11-19 11:51:48 +0100

    Set correct static legend image height and width
    Conflicts:
    	plugin/geomajas-layer-wms/wms/src/main/java/org/geomajas/layer/wms/WmsLayer.java

--- plugin/geomajas-layer-wms/wms/src/main/java/org/geomajas/layer/wms/WmsLayer.java
+++ plugin/geomajas-layer-wms/wms/src/main/java/org/geomajas/layer/wms/WmsLayer.java
@@ -12,6 +12,7 @@ package org.geomajas.layer.wms;
 
 import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
